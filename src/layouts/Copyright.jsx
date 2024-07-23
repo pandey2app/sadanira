a7b592495429fd1c74cec0db4cbe964af36cebaf
pandey2app <pandey2app@gmail.com>
2024-07-23 19:41:37 +0530

    changed layout of icon

--- src/layouts/Copyright.jsx
+++ src/layouts/Copyright.jsx
@@ -7,10 +7,10 @@ const Copyright = () => {
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        <span class="text-light"><Link to="/"><i class="fas fa-copyright text-light me-2"></i>team@sadanira</Link>, All right reserved.</span>
+                        <span class="text-light"><Link to="/"><i class="fas fa-copyright text-light me-2"></i> team@sadanira</Link>, All right reserved.</span>
                     </div>
                     <div class="col-md-6 my-auto text-center text-md-end text-white">
-                        Designed By <Link to='/' class="border-bottom" href="facebook.com">Nazish Ramish</Link> Distributed By <Link to='/' class="border-bottom" href="#">pandey2app</Link>
+                        Designed By <Link to='/' class="border-bottom" href="facebook.com"> Nazish Ramish</Link> Distributed By <Link to='/' class="border-bottom" href="#"> pandey2app</Link>
                     </div>
                 </div>
             </div>
