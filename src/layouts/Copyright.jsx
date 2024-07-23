812f38d51d6c2829c577a4e8cad3f11d30ee2b46
pandey2app <pandey2app@gmail.com>
2024-07-23 19:49:56 +0530

    copiright fonts fixed

--- src/layouts/Copyright.jsx
+++ src/layouts/Copyright.jsx
@@ -9,8 +9,8 @@ const Copyright = () => {
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
                         <span class="text-light"><Link to="/"><i class="fas fa-copyright text-light me-2"></i> team@sadanira</Link>, All right reserved.</span>
                     </div>
-                    <div class="col-md-6 my-auto text-center text-md-end text-white">
-                        Designed By <Link to='/' class="border-bottom" href="facebook.com"> Nazish Ramish</Link> Distributed By <Link to='/' class="border-bottom" href="#"> pandey2app</Link>
+                    <div class="col-md-6 my-auto text-center font-monospace text-sm-end text-white">
+                        Designed By <Link to='/' class="border-bottom" href="facebook.com"> Nazish Ramish</Link> Distributed By<Link to='/' class="border-bottom" href="#"> pandey2app</Link>
                     </div>
                 </div>
             </div>
