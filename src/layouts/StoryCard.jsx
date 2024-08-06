c57618cd8dc2d21f812f6e42fd730b15b99a98bd
nazishshaheen <nazishrapp@gmail.com>
2024-08-06 19:58:34 +0530

    update storyCard UI

--- src/layouts/StoryCard.jsx
+++ src/layouts/StoryCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import writingLogo from '%PUBLIC_URL%/img/writing'
+
 
 const StoryCard = () => {
     return (
         <section className=" d-flex flex-wrap justify-content-evenly">
-            <div className="card mb-3" style={{ maxWidth: "440px", height: '200px' }}>
+            <div className="card mb-3" style={{ maxWidth: "410px", height: '200px' }}>
                 <div className="row g-0" style={{ height: '100%' }}>
                     <div className="col-md-4" style={{ height: '100%' }}>
                         <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="rounded-start" alt="..." height={'100%'} />
@@ -13,10 +13,10 @@ const StoryCard = () => {
                     <div className="col-md-8">
                         <div className="card-body">
                             <h2 className="card-title">The Best Story title</h2>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p style={{ fontSize: '15px', padding: '2px' }}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             <div className='d-flex justify-content-between mt-n1'>
-                                <Link to='#'><img src={writingLogo} alt="" /><cite>Ramish Pandey</cite></Link>
-                                <Link to='#'>read more</Link>
+                                <Link to='#'><img src={'img/writing.png'} alt="" height={'20px'} className='me-1 mb-1' /><cite>Ramish Pandey</cite></Link>
+                                <Link to='#'>read <img src={'img/book.png'} alt="" height={'17px'} className='ms-1 mb-1' /></Link>
                             </div>
                         </div>
                     </div>
@@ -24,7 +24,7 @@ const StoryCard = () => {
             </div>
 
             {/*  */}
-            <div className="card mb-3" style={{ maxWidth: "440px", height: '200px' }}>
+            <div className="card mb-3" style={{ maxWidth: "410px", height: '200px' }}>
                 <div className="row g-0" style={{ height: '100%' }}>
                     <div className="col-md-4" style={{ height: '100%' }}>
                         <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="rounded-start" alt="..." height={'100%'} />
@@ -32,18 +32,18 @@ const StoryCard = () => {
                     <div className="col-md-8">
                         <div className="card-body">
                             <h2 className="card-title">The Best Story title</h2>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div className='d-flex justify-content-between'>
-                                <Link to='#'><cite>-Ramish Pandey</cite></Link>
-                                <Link to='#'>read more</Link>
+                            <p style={{ fontSize: '15px', padding: '2px' }}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div className='d-flex justify-content-between mt-n1'>
+                                <Link to='#'><img src={'img/writing.png'} alt="" height={'20px'} className='me-1 mb-1' /><cite>Ramish Pandey</cite></Link>
+                                <Link to='#'>read <img src={'img/book.png'} alt="" height={'17px'} className='ms-1 mb-1' /></Link>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-             {/*  */}
-             <div className="card mb-3" style={{ maxWidth: "440px", height: '200px' }}>
+            {/*  */}
+            <div className="card mb-3" style={{ maxWidth: "410px", height: '200px' }}>
                 <div className="row g-0" style={{ height: '100%' }}>
                     <div className="col-md-4" style={{ height: '100%' }}>
                         <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="rounded-start" alt="..." height={'100%'} />
@@ -51,18 +51,18 @@ const StoryCard = () => {
                     <div className="col-md-8">
                         <div className="card-body">
                             <h2 className="card-title">The Best Story title</h2>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div className='d-flex justify-content-between'>
-                                <Link to='#'><cite>-Ramish Pandey</cite></Link>
-                                <Link to='#'>read more</Link>
+                            <p style={{ fontSize: '15px', padding: '2px' }}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div className='d-flex justify-content-between mt-n1'>
+                                <Link to='#'><img src={'img/writing.png'} alt="" height={'20px'} className='me-1 mb-1' /><cite>Ramish Pandey</cite></Link>
+                                <Link to='#'>read <img src={'img/book.png'} alt="" height={'17px'} className='ms-1 mb-1' /></Link>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-             {/*  */}
-             <div className="card mb-3" style={{ maxWidth: "440px", height: '200px' }}>
+            {/*  */}
+            <div className="card mb-3" style={{ maxWidth: "410px", height: '200px' }}>
                 <div className="row g-0" style={{ height: '100%' }}>
                     <div className="col-md-4" style={{ height: '100%' }}>
                         <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="rounded-start" alt="..." height={'100%'} />
@@ -70,15 +70,52 @@ const StoryCard = () => {
                     <div className="col-md-8">
                         <div className="card-body">
                             <h2 className="card-title">The Best Story title</h2>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div className='d-flex justify-content-between'>
-                                <Link to='#'><cite>-Ramish Pandey</cite></Link>
-                                <Link to='#'>read more</Link>
+                            <p style={{ fontSize: '15px', padding: '2px' }}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div className='d-flex justify-content-between mt-n1'>
+                                <Link to='#'><img src={'img/writing.png'} alt="" height={'20px'} className='me-1 mb-1' /><cite>Ramish Pandey</cite></Link>
+                                <Link to='#'>read <img src={'img/book.png'} alt="" height={'17px'} className='ms-1 mb-1' /></Link>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div className="card mb-3" style={{ maxWidth: "410px", height: '200px' }}>
+                <div className="row g-0" style={{ height: '100%' }}>
+                    <div className="col-md-4" style={{ height: '100%' }}>
+                        <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="rounded-start" alt="..." height={'100%'} />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h2 className="card-title">The Best Story title</h2>
+                            <p style={{ fontSize: '15px', padding: '2px' }}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div className='d-flex justify-content-between mt-n1'>
+                                <Link to='#'><img src={'img/writing.png'} alt="" height={'20px'} className='me-1 mb-1' /><cite>Ramish Pandey</cite></Link>
+                                <Link to='#'>read <img src={'img/book.png'} alt="" height={'17px'} className='ms-1 mb-1' /></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card mb-3" style={{ maxWidth: "410px", height: '200px' }}>
+                <div className="row g-0" style={{ height: '100%' }}>
+                    <div className="col-md-4" style={{ height: '100%' }}>
+                        <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="rounded-start" alt="..." height={'100%'} />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h2 className="card-title">The Best Story title</h2>
+                            <p style={{ fontSize: '15px', padding: '2px' }}>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div className='d-flex justify-content-between mt-n1'>
+                                <Link to='#'><img src={'img/writing.png'} alt="" height={'20px'} className='me-1 mb-1' /><cite>Ramish Pandey</cite></Link>
+                                <Link to='#'>read <img src={'img/book.png'} alt="" height={'17px'} className='ms-1 mb-1' /></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </section >
     )
 }
