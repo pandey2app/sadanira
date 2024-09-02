d568c632335724455a3408324a4cbfcdcd23d65c
pandey2app <pandey2app@gmail.com>
2024-09-02 11:49:21 +0530

    fixed members page for error when no member

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -7,24 +7,28 @@ const Members = () => {
   const dispatch = useDispatch()
   const users = useSelector((state) => state.user.users)
 
-  const getUsers = useCallback(()=>{
+  const getUsers = useCallback(() => {
     dispatch(getUserStart())
-  },[dispatch])
-  useEffect(() =>{
+  }, [dispatch])
+  useEffect(() => {
     getUsers()
-  },[getUsers])
+  }, [getUsers])
   return (
     <>
-      <section className='p-4 '>
-      <h3 className='text-center px-2 mb-3 text-success text-bold'>Total {users.length} members are available now on sadaneera!</h3>
-      <section className='d-flex gap-3 flex-wrap'>
+      <section className='p-4 ' style={{ minHeight: "270px" }}>
         {
-          users.length > 0 && users.map((member, index) => (
-            <UserProfileCard key={index} creator={member} />
-          ))
+          users.length > 0 ?
+            <h3 className='text-center px-2 mb-3 text-success text-bold'>Total {users.length} members are available now on sadaneera!</h3> :
+            <h3 className='text-center px-2 mb-3 text-danger text-bold'>Sorry! No Active member available now on sadaneera!</h3>
         }
+        <section className='d-flex gap-3 flex-wrap '>
+          {
+            users.length > 0 && users.map((member, index) => (
+              <UserProfileCard key={index} creator={member} />
+            ))
+          }
+        </section>
       </section>
-    </section>
     </>
   )
 }
