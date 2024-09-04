a8007780c98e1f60b6297e7dab6b8516eb6a63fc
pandey2app <pandey2app@gmail.com>
2024-09-04 14:20:01 +0530

    added strict condition for get user call

--- src/hooks/useGetUser.js
+++ src/hooks/useGetUser.js
@@ -1,24 +1,28 @@
 import { useCallback, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUserStart } from '../redux/actions/user.action'
+import { useNavigate } from 'react-router-dom'
 
 const useGetUser = () => {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     const user = useSelector(state => state.user.currentUser)
     const getUserErrorMessage = useSelector(state => state.user.getUserErrorMessage)
-    // const isLoggedIn = useSelector(state => state.user.isLoggedIn)
+    const isLoggedIn = useSelector(state => state.user.isLoggedIn)
 
     const getUser = useCallback(() => {
         dispatch(getUserStart())
     }, [dispatch])
 
     useEffect(() => {
-        if(localStorage.getItem('isLoggedIn')){
-            setTimeout(()=>{
-                getUser()
-            },500)
+        if(localStorage.getItem('isLoggedIn') && isLoggedIn){
+            getUser()
+        }else{
+            localStorage.removeItem("isLoggedIn")
+            navigate('/login')
+            return null
         }
-    }, [getUser]);
+    }, [getUser, isLoggedIn, navigate]);
 
     if(user.name){
         return user
