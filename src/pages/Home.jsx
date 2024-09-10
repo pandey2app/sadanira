00a280121c43d08abdeaf78cbbfd38f07ddfb0e1
pandey2app <pandey2app@gmail.com>
2024-09-10 14:42:47 +0530

    changed home interface for Registered user

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Hero from '../layouts/Hero'
 import AboutSection from '../layouts/AboutSection'
 import FactSection from '../layouts/FactSection'
@@ -8,20 +8,34 @@ import TeamSection from '../layouts/TeamSection'
 import JoinUs from '../layouts/JoinUs'
 import TeamLead from '../layouts/TeamLead'
 import Testimonial from '../layouts/Testimonial'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+    const user = useSelector(state=> state.user.currentUser)
+
+    useEffect(() =>{
+        console.dir(user);
+    },[user])
+
+    if(!user){
+        return(
+            <>
+                <Hero />
+                <AboutSection />
+                <ServicesSection />
+                <FactSection />
+                <EventsSection />
+                <TeamSection />
+                <JoinUs />
+                <TeamLead />
+                <Testimonial />
+            </>
+        )
+    }
     return (
         <>
-            <Hero />
-            <AboutSection />
-            <ServicesSection />
-            <FactSection />
-            <EventsSection />
-            <TeamSection />
-            <JoinUs />
+            
             <TeamLead />
-            <Testimonial />
-            {/* <BlogSection /> */}
         </>
     )
 }
