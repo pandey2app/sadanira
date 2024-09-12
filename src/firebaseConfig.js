6ffa4c3c74eb95eb77c88757000516459d28cc4a
pandey2app <pandey2app@gmail.com>
2024-09-12 17:31:59 +0530

    added  . env  for firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBnaCPH8nUKnUhqOkn8P7lZCPiuBb9t6HY",
-    authDomain: "sadaneera-15fac.firebaseapp.com",
-    projectId: "sadaneera-15fac",
-    storageBucket: "sadaneera-15fac.appspot.com",
-    messagingSenderId: "982910592877",
-    appId: "1:982910592877:web:16370b0dd16a8d6f455c64"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
 // Initialize Firebase
