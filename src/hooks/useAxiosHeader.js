4116cf68d2af5ff3d2a9bbc263a4745b45176911
pandey2app <pandey2app@gmail.com>
2024-09-12 17:22:09 +0530

    added use axios

---
+++ src/hooks/useAxiosHeader.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const useAxiosHeader = ()=>{
+    const [progress, setProgress] = useState(null)
+
+    let multipartWithProgress = {
+        headers: {
+            'Content-Type': 'multipart/form-data',  // Important to set content-type
+        },
+        onUploadProgress: (progressEvent) => {
+            const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+            console.log(`Upload progress: ${percentCompleted}%`);
+            setProgress(percentCompleted) 
+        }
+    }
+
+    return [multipartWithProgress, progress, setProgress]
+}
+
+export default useAxiosHeader;
